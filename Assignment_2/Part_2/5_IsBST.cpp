f4d183e014ce41a688771ba5c2b93438a9e0c22e
Ángel Rosado <111909689+Liuxito2040@users.noreply.github.com>
2023-05-03 22:29:00 -0600

    Add files via upload
    finished

---
+++ Assignment_2/Part_2/5_IsBST.cpp
@@ -0,0 +1,214 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+	int value;
+	Node *_left, *_right;
+};
+
+class BinarySearchTree{
+	
+	public:
+	
+	Node* root = new Node;
+	
+	//function that searches for the node with the minimum value
+	int minimumValue(){
+		Node* currentNode = root;
+		
+		while(currentNode -> _left != NULL){
+			currentNode = currentNode -> _left;
+		}
+		
+		return currentNode -> value;
+	}
+		
+		
+	//function that searches for the node with the maximum value
+	int maximumValue(){
+		Node* currentNode = root;
+			
+		while(currentNode -> _right != NULL){
+			currentNode = currentNode -> _right;
+		}
+			
+		return currentNode -> value;
+	}
+			
+	bool containsThisValue(int val){
+		Node* currentNode = new Node;
+		currentNode = root;
+				
+		while(true){
+			//checking if the currentNode value it's equal to the sought value
+			if(currentNode -> value == val)
+				return true;
+					
+			//If I should go left
+			if(val < currentNode -> value){ 
+				//the sought value does not exist in the BST
+				if(currentNode -> _left == NULL)
+					return false;
+						
+				//updating the current Node
+				currentNode = currentNode -> _left;
+						
+			}else{ //If I should go right
+				if(currentNode -> _right == NULL)
+					return false;
+				
+				//updating the current Node
+				currentNode = currentNode -> _right;
+			}
+			
+		}
+		
+	}
+				
+				
+	//helper functions for insertNode function
+		
+	void createNodeAtLeft(Node* loc, int val){
+		Node* newNode = new Node;
+			
+		//initializing the newNode attributes
+		newNode -> value = val;
+		newNode -> _left = NULL;
+		newNode -> _right = NULL;
+			
+		//linking the newNode with the BST
+		loc -> _left = newNode;
+			
+		return;
+	}
+			
+	void createNodeAtRight(Node* loc, int val){
+		Node* newNode = new Node;
+			
+		//initializing the newNode attributes 
+		newNode -> value = val;
+		newNode -> _left = NULL;
+		newNode -> _right = NULL;
+			
+		//linking the newNode with the BST
+		loc -> _right = newNode;
+			
+		return;
+	}
+			
+	//function that insert a new Node in the BST
+	void insertNode(int val){
+		Node* currentNode = root;
+			
+		//A BST has the property that, in a certain node, the value of the left child of this
+		//node should be lesser, and the value of the right child should be
+		//greater. We must use this information to insert a node so, basically, we
+		//just check if the value that we need to insert in a new node is greater
+		//or lesser than the value of the current node and go to the left or right
+		//child with this information until the place where we must go does not
+		//exist, then we create the new node.
+			
+		while(true){
+			//if I should go left
+			if(val < currentNode -> value){
+				//Does the left child exist?
+				if(currentNode -> _left != NULL){
+					currentNode = currentNode -> _left;
+				}else{
+					createNodeAtLeft(currentNode, val);
+					break;
+				}
+				
+			}else{ // if I should go right
+				
+				//Does the right child exist?
+				if(currentNode -> _right != NULL){
+					currentNode = currentNode -> _right;
+				}else{
+					createNodeAtRight(currentNode, val);
+					break;
+				}				
+			}
+			
+		}
+			
+		return;
+	}
+					
+	//helper functions for testing
+		
+	//initializes the attributes of the root 
+	void getRoot(int value){
+			
+		root -> value = value;
+		root -> _left = NULL;
+		root -> _right = NULL;
+		
+		return;
+	}
+		
+	void inOrder(Node* currentNode){
+		if(currentNode == NULL)
+			return;
+		
+		inOrder(currentNode -> _left);
+		cout<<currentNode->value<<" ";
+		inOrder(currentNode -> _right);
+		
+		return;
+	}
+		
+};
+
+bool IsBST(Node* currentNode){
+	if(currentNode -> _left == NULL && currentNode -> _right == NULL)
+		return true;
+	
+	if(currentNode -> _left == NULL && currentNode -> _right -> value > currentNode -> value)
+		return IsBST(currentNode -> _right);
+	
+	if(currentNode -> _left -> value < currentNode -> value && currentNode -> _right == NULL)
+		return IsBST(currentNode -> _left);
+		
+	if(currentNode -> _left -> value < currentNode -> value && currentNode -> _right -> value > currentNode -> value)
+		return IsBST(currentNode -> _right) && IsBST(currentNode -> _left);
+	
+	return false;
+}
+
+void test(){
+	BinarySearchTree* myTree = new BinarySearchTree;
+	
+	//creating myTree
+	myTree -> getRoot(13);
+	myTree -> insertNode(10);
+	myTree -> insertNode(1);
+	myTree -> insertNode(14);
+	myTree -> insertNode(12);
+	myTree -> insertNode(16);
+	myTree -> insertNode(17);
+	myTree -> insertNode(18);
+	myTree -> insertNode(5);
+	
+	
+	cout<<"This is the tree: ";
+	myTree -> inOrder(myTree -> root);
+	
+	if(IsBST(myTree -> root))
+		cout<<"\nIt is a binary tree";
+	else
+		cout<<"\nIt is not a binary tree";
+	
+	//freeing memory
+	delete myTree;
+	
+	return;
+}
+
+int main() {
+	
+	test();
+	
+	return 0;
+}
+
